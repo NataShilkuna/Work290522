5342d27e8dcd1622c2b5428f6b68ce34145927e4
Natalia <Deva2014@yandex.ru>
2022-06-03 18:55:58 +0300

    Задача 52

---
+++ Task_5/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 52: Задайте двумерный массив из целых чисел.
+// Найдите среднее арифметическое элементов в каждом
+// столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого
+// столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.WriteLine("Введите количество строк массива: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов массива: ");
+int n = int.Parse(Console.ReadLine());
+
+
+int[,] GetArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(-10, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
+
+double CetSum(int[,] array)
+{
+    int[] sum = new int[n];
+    
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        sum[j] += array[i+j]; 
+        double avg = (double)sum[i] / n;
+        Console.Write(avg + ";");
+    }
+return avg;
+}
+}
+
+
+int[,] arrayResult = GetArray(m, n);
+PrintArray(arrayResult);
+Console.WriteLine();
+Console.WriteLine(CetSum(arrayResult));
